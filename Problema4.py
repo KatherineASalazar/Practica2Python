95e7fd05584f455f5ada6871f2b17aea13a784b1
KatherineASalazar <82777554+KatherineASalazar@users.noreply.github.com>
2024-02-03 18:26:32 +0000

    subiendo primera parte

--- Problema4.py
+++ Problema4.py
@@ -8,3 +8,20 @@
 #}
 #Una vez completado el ingreso de los datos, su programa debe mostrar en pantalla el listado
 #completo de los alumnos.
+
+
+num_alumnos = int(input("Ingrese el número de alumnos: "))
+
+lista_alumnos = []
+
+for i in range(num_alumnos):
+    nombre_alumno = input(f"Ingrese el nombre del alumno {i + 1}: ")
+    calificaciones = [float(input(f"Ingrese la calificación {j + 1} para {nombre_alumno}: ")) for j in range(3)]
+
+    alumno = {"Alumno": nombre_alumno, "Notas": calificaciones}
+    lista_alumnos.append(alumno)
+
+print("\nListado completo de alumnos:")
+for alumno in lista_alumnos:
+    print(alumno)
+
--- Problema5.py
+++ Problema5.py
@@ -4,3 +4,24 @@
 #Ejemplo:
 #Número ingresado: 15789000 y Dígito: 0
 #Cantidad de veces 0 en el número: 4
+
+numero = input("Ingresar un número de 6 dígitos:")
+digito = input("Ingresar un número de 1 dígito:")
+
+#Función
+def contar_digitos(numero, digito):
+
+    # Convertir el número a una cadena para poder usar count
+    str_numero = str(numero)
+
+    cantidad = str_numero.count(str(digito))
+
+    return cantidad
+
+
+# Llamada a la función
+resultado = contar_digitos(numero, digito)
+
+# Resultado
+print(f"Número ingresado: {numero} y Dígito: {digito}")
+print(f"Cantidad de veces {digito} en el número: {resultado}")
